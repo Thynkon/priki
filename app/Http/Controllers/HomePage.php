bddcbbbab7b9eae3d46e556e00c3d35be649e7f4
thynkon <thynkon@protonmail.com>
2021-11-25 09:54:40 +0100

    Show practices list and install tailwind

---
+++ app/Http/Controllers/HomePage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Practice;
+use App\Models\PublicationState;
+use Carbon\Carbon;
+
+class HomePage extends Controller
+{
+    public function index()
+    {
+        $date = Carbon::now();
+        $date->timezone(new \DateTimeZone('UTC'));
+        $date->subDays(5);
+        $state = PublicationState::where('slug', 'PUB')->get()[0];
+
+        $practices = Practice::where([
+            ['publication_state_id', $state->id],
+            ['updated_at', '<=', $date->toDateTimeString()],
+        ])->get();
+
+        return view('homepage')->with(['practices' => $practices]);
+    }
+}
