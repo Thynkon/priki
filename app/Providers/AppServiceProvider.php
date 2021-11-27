9ea1d19847a792a7d762c62cbe1995f8fc4893de
thynkon <thynkon@protonmail.com>
2021-11-27 15:03:30 +0100

    Create missing folders required by laravel to install packages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // for some reason, sactun has a migration called 2019_12_14_000001_create_personal_access_tokens_table.php
+        // under /vendor. this causes problems because jetstream also provides a migration with the same name
+        Sanctum::ignoreMigrations();
     }
 
     /**
