12dca6b84fa4e14f03ee94c65f70ce245d83e2ec
thynkon <thynkon@protonmail.com>
2022-01-27 15:33:28 +0100

    Show list of practices modifications

--- resources/views/practices/update.blade.php
+++ resources/views/practices/update.blade.php
@@ -38,7 +38,6 @@
                         @empty
                             <div>Aucune opinion à afficher</div>
                         @endforelse
-
                     </div>
 
                     <div class="flex flex-col md:flex-row flex-wrap">
@@ -55,6 +54,55 @@
                             <div class="p-4 bg-red-100 w-full text-center rounded">Vous devez vous connecter afin de pouvoir commenter des opinions !</div>
                         </div>
                     @endif
+                        @if ($practice->changelogs->count() > 0)
+                            <div class="p-10 w-full">
+                                <p class="font-bold mb-4">{{ __('Historique des modifications') }}</p>
+                                <div class="flex flex-col">
+                                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                          <table class="min-w-full divide-y divide-gray-200">
+                                            <thead class="bg-gray-50">
+                                              <tr>
+                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                  Personne
+                                                </th>
+                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                  Modifiée le
+                                                </th>
+                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                  Raison
+                                                </th>
+                                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                  Ancien titre
+                                                </th>
+                                              </tr>
+                                            </thead>
+                                            <tbody class="bg-white divide-y divide-gray-200">
+                                                @foreach ($practice->changelogs as $changelog)
+                                                  <tr>
+                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                      {{ $changelog->fullname }}
+                                                    </td>
+                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                      {{ $changelog->changelogs->updated_at->translatedFormat('jS F Y') }}
+                                                    </td>
+                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                      {{ $changelog->changelogs->reason }}
+                                                    </td>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-sm">
+                                                      {{ $changelog->changelogs->previously }}
+                                                    </td>
+                                                  </tr>
+                                                @endforeach
+                                            </tbody>
+                                          </table>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                        @endif
                     </div>
                 </div>
             </div>
